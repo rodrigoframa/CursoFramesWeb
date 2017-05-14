6a3b51c8b1cece5b3c15c0d2ad21ea7401dc799f
Rodrigo Frazão Maia <rodrigoframa@gmail.com>
2017-05-13 23:24:45 -0300

    API do Ciclo de Pagamento pronta

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,7 +1,7 @@
 const port = 3003
 
 const bodyParser = require('body-parser')
-const express = require('express');
+const express = require('express')
 const server = express()
 
 server.use(bodyParser.urlencoded({ extended: true }))
@@ -10,3 +10,5 @@ server.use(bodyParser.json())
 server.listen(port , function () {
   console.log(`BACKEND is running on port ${port}.`)
 })
+
+module.exports = server
