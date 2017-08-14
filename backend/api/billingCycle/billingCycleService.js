d1e7734fc32a3e9f66c98ebdf6dd2a2784fab2a0
Rodrigo Frazão Maia <rodrigoframa@gmail.com>
2017-08-14 00:04:33 -0300

    app finalizada

--- backend/api/billingCycle/billingCycleService.js
+++ backend/api/billingCycle/billingCycleService.js
@@ -4,24 +4,28 @@ const BillingCycle = require('./billingCycle');
 BillingCycle.methods(['get', 'post', 'put', 'delete']);
 BillingCycle.updateOptions({new : true, runValidators: true });
 
-BillingCycle.after('post', sendErrorsOrNext).after('put', sendErrorsOrNext);
+BillingCycle.after('post', saveSummary).after('put', saveSummary);
 
- function sendErrorsOrNext(req, res, next) {
+ function saveSummary(req, res, next) {
    const bundle = res.locals.bundle
 
    if(bundle.errors) {
      var errors = parseErrors(bundle.errors)
      res.status(500).json({errors})
    } else {
-     next()
+     const summary = {
+       billingCycle: bundle._id,
+       credit: _.sumBy(bundle.credits, 'value'),
+       debt: _.sumBy(bundle.debts, 'value')
+     }
    }
- };
+ }
 
 function parseErrors(nodeRestfulErrors) {
   const errors = []
   _.forIn(nodeRestfulErrors, error => errors.push(error.message))
   return errors
-};
+}
 
 
 BillingCycle.route('count', function (req,res,next) {
@@ -31,6 +35,6 @@ BillingCycle.route('count', function (req,res,next) {
     } else {
       res.json({value})
     }
-  });
-});
+  })
+})
 module.exports = BillingCycle;
